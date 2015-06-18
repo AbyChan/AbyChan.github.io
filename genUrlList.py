c30e7ef9a22e5aa15f48682869aaedc0c4998bde
AbyChan <crystal.tyan@gmail.com>
2015-06-19 01:43:16 +0800

    update

--- genUrlList.py
+++ genUrlList.py
@@ -1,25 +1,49 @@
 #coding=utf-8
 #!/usr/bin/python
 
-from lxml import etree
-tree = etree.parse("public/sitemap.xml")
-root = tree.getroot()
+def getArchiveUrl(filepath):
+    rst = []
+    with open(filepath) as f:
+        for line in f:
+            rst.append(line)
+    return rst
 
-rstFile = open('public/sitelist.txt', 'w+')
-urls = []
-for url in root:
-    for child in url:
-        if child.tag == '{http://www.sitemaps.org/schemas/sitemap/0.9}loc':
-            urls.append(child.text + '\n')
-            rstFile.write(child.text + '\n')
-rstFile.close()
 
-body = ''.join(urls)
-print body
+def readSiteMap(filepath):
+       from lxml import etree
+       tree = etree.parse(filepath)
+       root = tree.getroot()
 
-import httplib, urllib
-headers = {"Content-type": "text/plain",  "Accept": "text/plain", 'User-Agent': 'curl/7.12.1' }
-conn = httplib.HTTPConnection("data.zz.baidu.com")
-conn.request("POST", "/urls?site=tyan.io&token=2Po8M2T4ijaQqxed", body)
-response = conn.getresponse()
-print response.status, response.reason + '\n', response.read()
+       rstFile = open('public/sitelist.txt', 'w+')
+       urls = []
+       for url in root:
+           for child in url:
+               if child.tag == '{http://www.sitemaps.org/schemas/sitemap/0.9}loc':
+                   urls.append(child.text + '\n')
+                   rstFile.write(child.text + '\n')
+       rstFile.close()
+       #return ''.join(urls)
+       return urls
+
+def getNewUrl(archiveUrl, urllib):
+    rst = []
+    for url in urllib:
+        if url not in archiveUrl:
+            rst.append(url)
+    return rst
+
+def push2Baidu(body):
+    import httplib, urllib
+    headers = {"Content-type": "text/plain",  "Accept": "text/plain", 'User-Agent': 'curl/7.12.1' }
+    conn = httplib.HTTPConnection("data.zz.baidu.com")
+    conn.request("POST", "/urls?site=tyan.io&token=2Po8M2T4ijaQqxed", body)
+    response = conn.getresponse()
+    print response.status, response.reason + '\n', response.read()
+
+if __name__ == '__main__':
+    archiveUrl = getArchiveUrl('urlarchive')
+    urllist = readSiteMap('public/sitemap.xml')
+    newUrl = getNewUrl(archiveUrl, urllist)
+
+
+
